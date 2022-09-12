edeb77c9f4b1059fce3df89e512879223960862b
Martin Dallos <tinypin14@gmail.com>
2022-09-12 21:11:44 +0200

    Added background. Added new book form and add book functionality

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// * Wrappers, modals & buttons:
 const bookCardWrapper = document.querySelector('.book-cards');
 const addNewBookBtn = document.querySelector('.add-book-btn');
-const addBookFormModal = document.querySelector('.add-book-form-modal')
+const addBookFormModal = document.querySelector('.add-book-form-modal');
+const confirmNewBookBtn = document.querySelector('.confirm-book-btn');
+
+// * Form elements:
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readInput = document.querySelector('#read');
+
 let myLibrary = [];
 
 function Book(title, author, numOfPages, isItRead) {
@@ -20,6 +29,8 @@ function addBookToPage(title, author, numOfPages, isItRead) {
   <h3>${author}</h3>
   </div>
   <p>The book is ${numOfPages} pages long.</p>
+  <button type="button">Read the book</button>
+  <button type="button">Remove book</button>
   </div>
   `; 
 
@@ -35,11 +46,16 @@ function addBookToLibrary(title, author, numOfPages, isItRead) {
 function handleAddNewBookBtn(e) {
   console.log(e);
   addBookFormModal.classList.remove('hidden')
+}
 
+function handleConfirmNewBook(e) {
+  addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+  addBookFormModal.classList.add('hidden');
 }
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkein', 345, true);
 addBookToLibrary('The Fellowship of the Ring', 'J.R.R. Tolkein', 564, true);
 
 console.log(myLibrary);
-addNewBookBtn.addEventListener('click', handleAddNewBookBtn)
+addNewBookBtn.addEventListener('click', handleAddNewBookBtn);
+confirmNewBookBtn.addEventListener('click', handleConfirmNewBook);
