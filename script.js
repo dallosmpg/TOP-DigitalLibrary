4f6755c1c1d2da9d0cec923c45c905477178d526
Martin Dallos <tinypin14@gmail.com>
2022-09-12 20:27:17 +0200

    Redid the styling and refactored the js code. Added addNewBook function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const addBookBtn = document.querySelector('.add-book-btn');
-const addBookToLibraryBtn = document.querySelector('.add-book-to-libraray-btn')
-const bookInputForm = document.querySelector('.book-input');
-const addBookForm = document.querySelector('.add-book-form');
+const bookCardWrapper = document.querySelector('.book-cards');
+const addNewBookBtn = document.querySelector('.add-book-btn');
+const addBookFormModal = document.querySelector('.add-book-form-modal')
 let myLibrary = [];
 
 function Book(title, author, numOfPages, isItRead) {
@@ -9,25 +8,38 @@ function Book(title, author, numOfPages, isItRead) {
     this.author = author;
     this.numOfPages = numOfPages;
     this.isItRead = isItRead;
+    this.arrayID = myLibrary.length;
+}
+
+function addBookToPage(title, author, numOfPages, isItRead) {
+  const bookCardTemplate = `
+  <div class="book-card">
+  <div>
+  <h2>${title}</h2>
+  <p>by</p>
+  <h3>${author}</h3>
+  </div>
+  <p>The book is ${numOfPages} pages long.</p>
+  </div>
+  `; 
+
+  bookCardWrapper.innerHTML += bookCardTemplate
 }
 
 function addBookToLibrary(title, author, numOfPages, isItRead) {
     const newBook = new Book(title, author, numOfPages, isItRead)
     myLibrary.push(newBook);
+    addBookToPage(title, author, numOfPages, isItRead);
 }
 
-function handleAddBookBtn() {
-  console.log('test');
-  bookInputForm.classList.remove('hidden');
-}
-
-function handleAddBookToLibrary(e) {
+function handleAddNewBookBtn(e) {
   console.log(e);
+  addBookFormModal.classList.remove('hidden')
+
 }
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkein', 345, true);
 addBookToLibrary('The Fellowship of the Ring', 'J.R.R. Tolkein', 564, true);
 
 console.log(myLibrary);
-addBookBtn.addEventListener('click', handleAddBookBtn);
-addBookForm.addEventListener('submit', handleAddBookToLibrary)
+addNewBookBtn.addEventListener('click', handleAddNewBookBtn)
