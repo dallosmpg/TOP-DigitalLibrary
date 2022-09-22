c9c763957b9203c39a5cf70a6520a1927bd56ba6
Martin Dallos <tinypin14@gmail.com>
2022-09-22 23:31:37 +0200

    Tried several methods to solv event listener on created cards

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const bookCardWrapper = document.querySelector('.book-cards');
 const addNewBookBtn = document.querySelector('.add-book-btn');
 const addBookFormModal = document.querySelector('.add-book-form-modal');
 const confirmNewBookBtn = document.querySelector('.confirm-book-btn');
-const removeBookBtn = document.querySelector('.remove-book-btn');
 
 
 // * Form elements:
@@ -12,8 +11,10 @@ const authorInput = document.querySelector('#author');
 const pagesInput = document.querySelector('#pages');
 const readInput = document.querySelector('#read');
 
+// * Book holder array
 let myLibrary = [];
 
+// * Book constructor function
 function Book(title, author, numOfPages, isItRead) {
     this.title = title;
     this.author = author;
@@ -21,6 +22,8 @@ function Book(title, author, numOfPages, isItRead) {
     this.isItRead = isItRead;
     this.arrayID = myLibrary.length;
 }
+
+// * Book prototype functions
 Book.prototype.handleRemoveBookBtn = function (e) {
   console.log(e);
   console.log('It works!');
@@ -29,37 +32,39 @@ Book.prototype.handleReadBookBtn = function (e) {
   console.log(e);
 }
 
+// * Book card creating function
 function addBookToLibrary(title, author, numOfPages, isItRead) {
     const newBook = new Book(title, author, numOfPages, isItRead);
     myLibrary.push(newBook);
 
     const arrayIndex = newBook.arrayID;
-    console.log(myLibrary[arrayIndex].title);
     const bookCardTemplate = `
-  <div class="book-card" data-arrayID="${myLibrary[arrayIndex].arrayID}">
+  <div class="book-card-${arrayIndex} book-card" data-arrayID="${myLibrary[arrayIndex].arrayID}">
     <div>
       <h2>${myLibrary[arrayIndex].title}</h2>
       <p>by</p>
       <h3>${myLibrary[arrayIndex].author}</h3>
     </div>
     <p>The book is ${myLibrary[arrayIndex].numOfPages} pages long.</p>
-    <button type="button" class="read-book-btn">Read the book</button>
-    <button type="button" class="remove-book-btn" onclick="Book.prototype.handleRemoveBookBtn">Remove book</button>
+    <button type="button" class="read-book-btn-${arrayIndex}">Read the book</button>
+    <button type="button" class="remove-book-btn-${arrayIndex}">Remove book</button>
   </div>
   `;
-  bookCardWrapper.innerHTML += bookCardTemplate
+  bookCardWrapper.innerHTML += bookCardTemplate;
+  document.querySelector(`.remove-book-btn-${arrayIndex}`).addEventListener('click', myLibrary[arrayIndex].handleRemoveBookBtn);
+  document.querySelector(`.read-book-btn-${arrayIndex}`).addEventListener('click', myLibrary[arrayIndex].handleReadBookBtn);
 }
 
+// * INput handler functions
 function handleAddNewBookBtn(e) {
   addBookFormModal.classList.remove('hidden');
 }
-
 function handleConfirmNewBook(e) {
   addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
   addBookFormModal.classList.add('hidden');
 }
 
-
+// * Event listeners
 console.log(myLibrary);
 addNewBookBtn.addEventListener('click', handleAddNewBookBtn);
 confirmNewBookBtn.addEventListener('click', handleConfirmNewBook);
