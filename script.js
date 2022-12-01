bbb887dccd4042b386143a82525b9be64bf9f379
Martin Dallos <tinypin14@gmail.com>
2022-12-01 13:42:19 +0100

    Commit and push for WebStorm

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// * Wrappers, modals & buttons:
 const bookCardWrapper = document.querySelector('.book-cards');
 const addNewBookBtn = document.querySelector('.add-book-btn');
 const addBookFormModal = document.querySelector('.add-book-form-modal');
 const confirmNewBookBtn = document.querySelector('.confirm-book-btn');
-
 
 // * Form elements:
 const titleInput = document.querySelector('#title');
@@ -16,15 +14,15 @@ const readBookBtnTemplate = `<button type="button" class="read-book-btn- read-bo
 const removeBookBtnTemplate = `<button type="button" class="remove-book-btn- remove-book-btn">Remove book</button>`;
 
 // * Book holder array
-let myLibrary = [];
+const myLibrary = [];
 
 // * Book constructor function
 function Book(title, author, numOfPages, isItRead, bookID) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.isItRead = isItRead;
-    this.bookID = bookID;
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.isItRead = isItRead;
+  this.bookID = bookID;
 }
 
 // * Book button functions
@@ -56,11 +54,11 @@ function handleUnreadBookBtn(e) {
   e.currentTarget.parentElement.classList.remove('the-book-is-read');
   const bookID = e.currentTarget.parentElement.dataset.arrayid;
 
-  const rmBtn = document.createElement('button'); 
-  const rdBtn = document.createElement('button');  
+  const rmBtn = document.createElement('button');
+  const rdBtn = document.createElement('button');
   rdBtn.textContent = 'Read the book';
   rdBtn.classList.add(`read-book-btn-${bookID}`, `read-book-btn`);
-  rmBtn.textContent = 'Remove book'; 
+  rmBtn.textContent = 'Remove book';
   rmBtn.classList.add(`remove-book-btn-${bookID}`, 'remove-book-btn');
   e.currentTarget.parentElement.appendChild(rdBtn);
   e.currentTarget.parentElement.appendChild(rmBtn);
@@ -72,27 +70,31 @@ function handleUnreadBookBtn(e) {
 }
 
 function attachEventListenerToBookBtns() {
-  document.querySelectorAll('.read-book-btn').forEach(book => book.addEventListener('click', handleReadBookBtn));
-  document.querySelectorAll('.remove-book-btn').forEach(book => book.addEventListener('click', handleRemoveBookBtn));
+  document
+    .querySelectorAll('.read-book-btn')
+    .forEach((book) => book.addEventListener('click', handleReadBookBtn));
+  document
+    .querySelectorAll('.remove-book-btn')
+    .forEach((book) => book.addEventListener('click', handleRemoveBookBtn));
 }
 
 // * Book card creating function
 function addBookToLibrary(title, author, numOfPages, isItRead) {
-    const bookID = myLibrary.length;
+  const bookID = myLibrary.length;
 
-    const newBook = new Book(title, author, numOfPages, isItRead, bookID);
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, numOfPages, isItRead, bookID);
+  myLibrary.push(newBook);
 
-    const bookCardTemplate = `
+  const bookCardTemplate = `
   <div class="book-card-${bookID} book-card" data-arrayID="${bookID}">
-    <div>
+    <div class="book-content">
       <h2>${title}</h2>
       <p>by</p>
       <h3>${author}</h3>
     </div>
     <p>The book is ${numOfPages} pages long.</p>
-    <button type="button" class="read-book-btn-${bookID} read-book-btn">Read the book</button>
-    <button type="button" class="remove-book-btn-${bookID} remove-book-btn">Remove book</button>
+      <button type="button" class="read-book-btn-${bookID} read-book-btn">Read the book</button>
+      <button type="button" class="remove-book-btn-${bookID} remove-book-btn">Remove book</button>
   </div>
   `;
   bookCardWrapper.innerHTML += bookCardTemplate;
@@ -104,7 +106,13 @@ function handleAddNewBookBtn(e) {
 }
 function handleConfirmNewBook(e) {
   const bookID = myLibrary.length + 1;
-  addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.value, bookID);
+  addBookToLibrary(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    readInput.value,
+    bookID
+  );
   attachEventListenerToBookBtns();
   addBookFormModal.classList.add('hidden');
 }
@@ -112,6 +120,7 @@ function handleConfirmNewBook(e) {
 // * Event listeners
 addNewBookBtn.addEventListener('click', handleAddNewBookBtn);
 confirmNewBookBtn.addEventListener('click', handleConfirmNewBook);
+console.log('🚀 ~ file: script.js:123 ~ confirmNewBookBtn', confirmNewBookBtn);
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkein', 345, true);
 addBookToLibrary('The Fellowship of the Ring', 'J.R.R. Tolkein', 564, true);
